d5b2efb37db5a256bac6edc1be8c6756fdc5dd25
newshell <52154054+newshell3125@users.noreply.github.com>
2022-10-24 16:03:20 +0900

    Ray準備終了

--- Unity-FPS-GunSword/Assets/Scripts/Player/Attack/PlayerShotScript.cs
+++ Unity-FPS-GunSword/Assets/Scripts/Player/Attack/PlayerShotScript.cs
@@ -1,29 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Diagnostics;
 using UnityEngine;
 
 public class PlayerShotScript : MonoBehaviour
 {
     public float rayDistance;
 
-    private void FixedUpdate()
+    private void Update()
     {
         Transform trans = transform;
 
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (Input.GetMouseButton(0))
         {
             var direction = trans.forward;
 
             Vector3 rayPosition = trans.position + new Vector3(0.0f, 0.0f, 0.0f);
-            Ray ray = new Ray(rayPosition, direction);
-            UnityEngine.Debug.DrawRay(rayPosition, direction * rayDistance, UnityEngine.Color.red);
+            Ray ray = Camera.main .ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(rayPosition, direction * -rayDistance, UnityEngine.Color.red);
 
             RaycastHit hit;
             if(Physics.Raycast(ray,out hit))
             {
-                UnityEngine.Debug.Log("HIT‚µ‚Ü‚µ‚½‚æ");
+                Debug.Log("HIT‚µ‚Ü‚µ‚½‚æ");
 
                 if(hit.collider.tag == "OBJECT")
                 {
